d9f76ddc66d994e7ee39ef5d1e9a6d59841ff74f
Cojocaru Ionut-Alexandru <alexandru.cojocaru@amazingsoft.ro>
2019-12-02 12:18:52 +0200

    create project

---
+++ Test1/Test1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test1
+//
+//  Created by Cojocaru Ionut Alexandru on 12/2/19.
+//  Copyright © 2019 Cojocaru Ionut Alexandru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
